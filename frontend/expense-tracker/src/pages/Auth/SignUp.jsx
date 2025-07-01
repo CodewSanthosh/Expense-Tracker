2475ea21c05cedd141e4c7dd1a918df1bd2ea8f1
Santhosh <k.santhosh050505@gmail.com>
2025-07-01 13:20:57 +0530

    Fix: corrected Input import in SignUp.jsx

--- frontend/expense-tracker/src/pages/Auth/SignUp.jsx
+++ frontend/expense-tracker/src/pages/Auth/SignUp.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import AuthLayout from "../../components/layouts/AuthLayout";
-import Input from "../../components/inputs/input";
+import Input from "../../components/inputs/Input";
 import ProfilePhotoSelector from "../../components/inputs/ProfilePhotoSelector";
 import { Link } from "react-router-dom";
 import { validateEmail } from "../../utils/helper";
